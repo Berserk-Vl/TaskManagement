ebdb0851f9d69cfd75f67666fb3c67031db11f20
Berserk-Vl <berkut.is.back@gmail.com>
2024-08-06 03:11:15 +0300

    Initial commit

---
+++ src/test/java/ru/sb/TaskManagment/TaskManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sb.TaskManagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
