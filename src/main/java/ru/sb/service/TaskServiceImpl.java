ed760c3d0823d4d7c3212abd7dd71dcc7099321f
Berserk-Vl <berkut.is.back@gmail.com>
2024-08-09 20:15:38 +0300

    Added utility methods and fields in TaskServiceImpl.java.

--- src/main/java/ru/sb/service/TaskServiceImpl.java
+++ src/main/java/ru/sb/service/TaskServiceImpl.java
@@ -4,12 +4,17 @@ import org.springframework.stereotype.Service;
 import ru.sb.model.Task;
 import ru.sb.model.TaskRepository;
 
+import java.util.Arrays;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 @Service
 public class TaskServiceImpl implements TaskService {
     private TaskRepository taskRepository;
     private UserService userService;
+    private static final int MAX_TITLE_LENGTH = 50;
+    private static final int MAX_DESCRIPTION_LENGTH = 300;
+    private static final int MAX_EMAIL_LENGTH = 30;
 
 
     public TaskServiceImpl(TaskRepository taskRepository, UserService userService) {
@@ -43,6 +48,78 @@ public class TaskServiceImpl implements TaskService {
             return Map.of("task", taskRepository.save(task));
         } catch (Exception e) {
             return Map.of("error", "Bad value / Missing value");
+        }
+    }
+
+    private Task getTask(Long taskId) throws NoSuchElementException{
+        if (taskRepository.findById(taskId).isPresent()) {
+            return taskRepository.findById(taskId).get();
+        }
+        throw new NoSuchElementException(String.format("ERROR: A task(%d) not exists.", taskId));
+    }
+
+    private void isAuthor(Task task, Map<String, String> fields) throws IllegalArgumentException {
+        if (!(fields.containsKey("author") && task.getAuthor().equals(fields.get("author")))) {
+            throw new IllegalArgumentException(String.format("ERROR: You are not an author of the task(%d).", task.getId()));
+        }
+    }
+
+    private void setTextField(Task task, Map<String, String> fields, String fieldName,
+                              int maxLength, boolean nullable, boolean required)
+            throws NullPointerException, NoSuchElementException, IllegalArgumentException {
+        if (fields.containsKey(fieldName)) {
+            if (fields.get(fieldName) != null) {
+                if (fields.get(fieldName).length() <= maxLength) {
+                    switch (fieldName) {
+                        case "title" -> task.setTitle(fields.get(fieldName));
+                        case "description" -> task.setDescription(fields.get(fieldName));
+                        case "author" -> task.setAuthor(fields.get(fieldName));
+                        case "performer" -> {
+                            if (userService.findUserByEmail(fields.get("performer")) != null) {
+                                task.setPerformer(fields.get(fieldName));
+                            } else {
+                                throw new IllegalArgumentException(
+                                        String.format("ERROR: Field(%s) can't be set, because user with specified email(%s) not exists.",
+                                                fieldName, fields.get(fieldName)));
+                            }
+                        }
+                    }
+                } else {
+                    throw new IllegalArgumentException(String.format("ERROR: Field(%s) exceeds max length(%d > %d).",
+                            fieldName, fields.get(fieldName).length(), maxLength));
+                }
+            } else if (nullable) {
+                switch (fieldName) {
+                    case "performer" -> task.setPerformer(fields.get(fieldName));
+                }
+            } else {
+                throw new NullPointerException(String.format("ERROR: Field(%s) can not be null.", fieldName));
+            }
+        } else if (required) {
+            throw new NoSuchElementException(String.format("ERROR: Field(%s) not found.", fieldName));
+        }
+    }
+
+    private void setEnumField(Task task, Map<String, String> fields, String fieldName,
+                              Object[] validValues, boolean nullable, boolean required)
+            throws NullPointerException, NoSuchElementException, IllegalArgumentException {
+        if (fields.containsKey(fieldName)) {
+            if (fields.get(fieldName) != null) {
+                try {
+                    switch (fieldName) {
+                        case "status" -> task.setStatus(Task.Status.valueOf(fields.get(fieldName).toUpperCase()));
+                        case "priority" -> task.setPriority(Task.Priority.valueOf(fields.get(fieldName).toUpperCase()));
+                    }
+                } catch (IllegalArgumentException e) {
+                    throw new IllegalArgumentException(String.format("ERROR: Invalid value for %s, valid values are %s.",
+                            fieldName, Arrays.toString(validValues)));
+                }
+            } else if (!nullable) {
+                throw new NullPointerException(String.format("ERROR: Field(%s) can not be null.", fieldName));
+            }
+
+        } else if (required) {
+            throw new NoSuchElementException(String.format("ERROR: Field(%s) not found.", fieldName));
         }
     }
 }
